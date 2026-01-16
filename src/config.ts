852bd305542c7961cbac056376f328e71c8caa60
Anixia8 <anixia.digregorio@studio.unibo.it>
2026-01-16 11:31:34 +0100

    modified config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "deploy-vm-test-function-ccf5g9gagff9fshy.westeurope-01.azurewebsites.net";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://deploy-vm-test-function-ccf5g9gagff9fshy.westeurope-01.azurewebsites.net";
